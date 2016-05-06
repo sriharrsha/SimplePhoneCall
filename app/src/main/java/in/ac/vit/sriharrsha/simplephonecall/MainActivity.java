1a543c1e4b67c5b52f3e47a68350e5b2874f73ff
Sri Harrsha <sriharrsha@live.com>
2016-05-06 09:25:35 +0530

    Initial commit

---
+++ app/src/main/java/in/ac/vit/sriharrsha/simplephonecall/MainActivity.java
@@ -0,0 +1,22 @@
+package in.ac.vit.sriharrsha.simplephonecall;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void makeCall(View view) {
+        Intent i=new Intent(Intent.ACTION_CALL);
+        i.setData(Uri.parse("tel:"+"012345678"));
+
+    }
+}
